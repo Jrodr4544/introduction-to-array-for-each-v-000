eabf15313fac5761a1c0627d0b8f428bfd92b542
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-06-30 14:48:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var doToElementsInArray = (array, callback) => {
   array.forEach(callback);
 }
 
-var callback = (element, index, array) => {
-
-}
-
 // Add your changeCompletely() function here:
 var changeCompletely = (element, index, array) => {
   array[index] = element + " is element # "+index;  
